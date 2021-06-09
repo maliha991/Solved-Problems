8b89a18d1acb409971f4920956d8a22e3f3f5500
Maliha Tabassum Mithila <maliha.mithila24@gmail.com>
2021-06-09 16:23:56 +0600

    Updated 1689

--- Leetcode/Medium/1689._Minimum_Deci_Binary.js
+++ Leetcode/Medium/1689._Minimum_Deci_Binary.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
 var minPartitions = function (n) {
-	let number = n.charAt(0);
+	let max = n.charAt(0);
 	for (let i = 1; i < n.length; i++) {
-		number = Math.max(number, n.charAt(i));
+		if (n.charAt(i) > max) {
+			max = n.charAt(i);
+		}
 	}
-	return number;
+	return max;
 };
