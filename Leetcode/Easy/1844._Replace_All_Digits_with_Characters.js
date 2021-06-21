275751ee8c761661bbd5a8ea8ba861146f1eea76
Maliha Tabassum Mithila <maliha.mithila24@gmail.com>
2021-06-21 23:48:17 +0600

    add 1844

--- Leetcode/Easy/1844._Replace_All_Digits_with_Characters.js
+++ Leetcode/Easy/1844._Replace_All_Digits_with_Characters.js
@@ -3,13 +3,12 @@
  * @return {string}
  */
 var replaceDigits = function (s) {
-	let output = s.split("");
-	output.forEach((value, index) => {
-		if (index % 2 != 0) {
-			output[index] = String.fromCharCode(
-				output[index - 1].charCodeAt() + parseInt(value)
-			);
-		}
-	});
-	return output.join("");
+	return s
+		.split("")
+		.map((value, index) => {
+			if (isNaN(value)) return value;
+			else
+				return String.fromCharCode(s[index - 1].charCodeAt() + parseInt(value));
+		})
+		.join("");
 };
